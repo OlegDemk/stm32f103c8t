c0adf2227bd81a70f88d21b4bda6dd76f279c255
OlegDemk <olegdemkiv111@gmail.com>
2020-09-26 23:11:20 +0300

    second commit

--- Castom_board/Core/Src/main.c
+++ Castom_board/Core/Src/main.c
@@ -193,7 +193,6 @@ int main(void)
         		HAL_Delay(500);
 			#endif
 
-
         	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
         	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
         	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
